1f7e72fe0107d10a3fdb8598c8fdc453ca75058a
Michael McClimon <michael@mcclimon.org>
2014-01-05 17:07:20 -0500

    Whitespace

--- lib/HappyJar.pm
+++ lib/HappyJar.pm
@@ -4,45 +4,45 @@ use v5.10;
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  $self->hook(before_dispatch => \&redirect_to_https);
+    $self->hook(before_dispatch => \&redirect_to_https);
 
-  # Router
-  my $r = $self->routes;
+    # Router
+    my $r = $self->routes;
 
-  $r->get('/')->to('default#index');
-  $r->get('/env')->to('default#env');
+    $r->get('/')->to('default#index');
+    $r->get('/env')->to('default#env');
 }
 
 sub redirect_to_https {
-  # gets the default controller as a parameter
-  my $c = shift;
+    # gets the default controller as a parameter
+    my $c = shift;
 
-  no warnings qw(uninitialized);
+    no warnings qw(uninitialized);
 
-  # set the HTTPS header correctly if it's forwarded
-  $c->req->url->base->scheme('https') if
-    $c->req->headers->header('x-forwarded-proto') eq 'https';
+    # set the HTTPS header correctly if it's forwarded
+    $c->req->url->base->scheme('https') if
+        $c->req->headers->header('x-forwarded-proto') eq 'https';
 
-  my $url = $c->req->url;
+    my $url = $c->req->url;
 
-  # if we're on 'localhost', HTTP is fine
-  my $host = $url->base->host || '';
-  return if lc $host eq 'localhost';
+    # if we're on 'localhost', HTTP is fine
+    my $host = $url->base->host || '';
+    return if lc $host eq 'localhost';
 
-  # otherwise, try to redirect to https
-  my $scheme = $url->base->scheme;
-  return if lc $scheme eq 'https';
+    # otherwise, try to redirect to https
+    my $scheme = $url->base->scheme;
+    return if lc $scheme eq 'https';
 
-  # reconstruct url
-  my $new = $url->base->clone();
-  $new->scheme('https');
-  $new->path($url->path);
-  $new->query($url->query);
-  $new->fragment($url->fragment);
+    # reconstruct url
+    my $new = $url->base->clone();
+    $new->scheme('https');
+    $new->path($url->path);
+    $new->query($url->query);
+    $new->fragment($url->fragment);
 
-  $c->redirect_to($new->to_string);
+    $c->redirect_to($new->to_string);
 }
 
 1;
--- lib/HappyJar/Default.pm
+++ lib/HappyJar/Default.pm
@@ -3,13 +3,13 @@ use Mojo::Base 'Mojolicious::Controller';
 
 # This action will render a template
 sub index {
-  my $self = shift;
-  $self->render(text => "Hello world!");
+    my $self = shift;
+    $self->render(text => "Hello world!");
 }
 
 sub env {
-  my $self = shift;
-  $self->render(inline => '<pre><%= dumper $self->tx->req %></pre>');
+    my $self = shift;
+    $self->render(inline => '<pre><%= dumper $self->tx->req %></pre>');
 }
 
 1;
